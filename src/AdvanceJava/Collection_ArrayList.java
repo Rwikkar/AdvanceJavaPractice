2833fef81ee7c24a80c8c07464eb15400e421a0f
Rwikkar <karsoubhik8@gmail.com>
2023-08-11 16:57:26 +0530

    Array List in Java

--- src/AdvanceJava/Collection_ArrayList.java
+++ src/AdvanceJava/Collection_ArrayList.java
@@ -13,7 +13,7 @@ public class Collection_ArrayList {
         arrayList.add(7); // Adding value in array list
         arrayList.add(0, 5); // To add a value 5 at index position 0 we can use this syntax
         arrayList.add(0, 9); // Now if we again add another value 9 at position index 0 then it will add 9 at 0th index and will push 0th element (here is 5) to 1st index. And so on...
-        for(int i = 0; i < arrayList.size(); i++) { // Accessing all elements in the array list
+        for(int i = 0; i < arrayList.size(); i++) { // Accessing all elements of the array list
             System.out.print(arrayList.get(i));
             System.out.print(", ");
         }
@@ -26,7 +26,7 @@ public class Collection_ArrayList {
         arrayList1.add(14); // Adding value in array list
         arrayList.addAll(arrayList1); // We can use add all method to add arrayList1 in arrayList
         System.out.println("Elements of arrayList after adding all elements of arrayList1 in it -->");
-        for(int i = 0; i < arrayList.size(); i++) { // Accessing all elements in the array list
+        for(int i = 0; i < arrayList.size(); i++) { // Accessing all elements of the array list
             System.out.print(arrayList.get(i));
             System.out.print(", ");
         }
@@ -36,12 +36,12 @@ public class Collection_ArrayList {
         System.out.println(arrayList1.indexOf(17)); // It will return the index value of 17. If 17 is not present in array list then it will return -1
         System.out.println(arrayList1.lastIndexOf(17)); // It will return the last index where 17 is present
         System.out.println(arrayList1.set(2, 437)); // It will replace the 2nd index value with 437
-        for(int i = 0; i < arrayList1.size(); i++) { // Accessing all elements in the array list
+        for(int i = 0; i < arrayList1.size(); i++) { // Accessing all elements of the array list
             System.out.print(arrayList1.get(i));
             System.out.print(", ");
         }
         arrayList.clear(); // It makes the array as empty array
-        for(int i = 0; i < arrayList.size(); i++) { // Accessing all elements in the array list
+        for(int i = 0; i < arrayList.size(); i++) { // Accessing all elements of the array list
             System.out.print(arrayList.get(i));
             System.out.print(", ");
         }
